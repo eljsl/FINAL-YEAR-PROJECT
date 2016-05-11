92dcf1f5980f3706c9cb49ec1556cddbb88e555c
Jamie Lamb <j.s.lamb-11@student.lboro.ac.uk>
2016-05-11 15:33:21 +0100

    dfhdfh

--- Matlab/08-02-2016/FINAL/TDOA/hej.m
+++ Matlab/08-02-2016/FINAL/TDOA/hej.m
@@ -1,7 +1,7 @@
 %somewhere set the initial guess at source location
 %Guess = [1,3,5];
 
-function rv = hej(Guess)
+function rv = hej(Guess,t1,t2,t3,t4)
 %
 % this is the function that returns the square deviations of the time 
 % differences from a source at the Guess position and the measured 
@@ -17,8 +17,7 @@ xyz = [0,2,2,0;
 v = 1482;
 
 % Time delays of pair 1-2,1-3 and 1-4
-[t1,t2,t3,t4]=get_time_lags()
-Measured_delay = [0.00012, 0.000104, 0.000328];
+Measured_delay = [0.00012,0.000104,0.000328];
 
 % Ranges from assumed location to microphones:
 
--- Matlab/08-02-2016/FINAL/TDOA/run_hej.m
+++ Matlab/08-02-2016/FINAL/TDOA/run_hej.m
@@ -3,6 +3,12 @@ close all
 
 Measured_delay = [0.00012, 0.000104, 0.000328];
 AllXYZ=zeros(3,100);
+[t1,t2,t3,t4]=get_time_lags()
+t12=abs(t2-t1);
+t13=abs(t3-t1);
+t14=abs(t4-t1);
+
+% hej(t1,t2,t3,t4);
 
 for n=1:1,
 randx=2*rand;
@@ -23,15 +29,27 @@ mx=XYZ(1);
 my=XYZ(2);
 mz=XYZ(3);
 
-scatter(xyz(1,1),xyz(2,1))
+scatter(xyz(1,1),xyz(2,1),'filled')
 hold on
-scatter(xyz(1,2),xyz(2,2))
+scatter(xyz(1,2),xyz(2,2),'filled')
 hold on
-scatter(xyz(1,3),xyz(2,3))
+scatter(xyz(1,3),xyz(2,3),'filled')
 hold on
-scatter(xyz(1,4),xyz(2,4))
+scatter(xyz(1,4),xyz(2,4),'filled')
 hold on
-scatter(mx,my,'r','filled')
+scatter(mx,my,80,'r','filled')
 grid on
 xlim([-1 3])
 ylim([-1 3])
+
+txt1 = 'Hydrophone 1 ';
+text(xyz(1,1),xyz(2,1),txt1,'VerticalAlignment','bottom','HorizontalAlignment','center')
+txt2 = 'Hydrophone 2 ';
+text(xyz(1,2),xyz(2,2),txt2,'VerticalAlignment','bottom','HorizontalAlignment','center')
+txt3 = 'Hydrophone 3 ';
+text(xyz(1,3),xyz(2,3),txt3,'VerticalAlignment','bottom','HorizontalAlignment','center')
+txt4 = 'Hydrophone 4 ';
+text(xyz(1,4),xyz(2,4),txt4,'VerticalAlignment','bottom','HorizontalAlignment','center')
+%Label source location
+txt5 = 'Source';
+text(mx,my,txt5,'VerticalAlignment','top','HorizontalAlignment','center')
